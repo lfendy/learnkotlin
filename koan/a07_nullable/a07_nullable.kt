08951175c88911d5cfe9439684b5b3b774d4de3c
Liauw Fendy <lfendy@some.place>
2020-01-21 10:29:00 +1100

    exercise 7 nullable

--- koan/a07_nullable/a07_nullable.kt
+++ koan/a07_nullable/a07_nullable.kt
@@ -1,6 +1,65 @@
+/*
+// Rewrite this into kotlin
+public void sendMessageToClient(
+    @Nullable Client client,
+    @Nullable String message,
+    @NotNull Mailer mailer
+) {
+    if (client == null || message == null) return;
 
+    PersonalInfo personalInfo = client.getPersonalInfo();
+    if (personalInfo == null) return;
+
+    String email = personalInfo.getEmail();
+    if (email == null) return;
+
+    mailer.sendMessage(email, message);
+}
+ */
+
+fun sendMessageToClient(
+    client: Client?, message: String?, mailer: Mailer
+){
+  println("sending")
+
+  val email = client?.personalInfo?.email
+  if(email == null || message == null) return
+
+  mailer.sendMessage(email, message)
+
+  println("end sending")
+  println("")
+}
+
+class Client (val personalInfo: PersonalInfo?)
+class PersonalInfo (val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
 
 fun main(args: Array<String> = arrayOf()) {
+  val mailer = object : Mailer {
+    override fun sendMessage(email: String, message: String): Unit =  println("Sending ${message} to ${email}")
+  }
+
+  val email = "some@where.out.there"
+  val message = "my message"
+
+  println("everythingIsOK")
+  sendMessageToClient(Client(PersonalInfo(email)), message, mailer)
+
+  println("noClient")
+  sendMessageToClient(null, message, mailer)
+
+  println("noMessage")
+  sendMessageToClient(Client(PersonalInfo(email)), null, mailer)
+
+  println("noPersonalInfo")
+  sendMessageToClient(Client(null), message, mailer)
+
+  println("noEmail")
+  sendMessageToClient(Client(PersonalInfo(null)), message, mailer)
+  println("")
   println("OK")
 }
 
