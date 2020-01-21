4534e0b4afc0ee12ae433b9acaa55207b67bc38a
Liauw Fendy <lfendy@some.place>
2020-01-21 11:05:50 +1100

    added TODO to project template, finished exercise 8

---
+++ koan/a08_smart_casts/a08_smart_casts.kt
@@ -0,0 +1,45 @@
+/*
+// translate into kotling
+public int eval(Expr expr) {
+    if (expr instanceof Num) {
+        return ((Num) expr).getValue();
+    }
+    if (expr instanceof Sum) {
+        Sum sum = (Sum) expr;
+        return eval(sum.getLeft()) + eval(sum.getRight());
+    }
+    throw new IllegalArgumentException("Unknown expression");
+}
+
+*/
+
+fun eval(expr: Expr): Int  = evalwhen(expr)
+
+fun evalif(expr: Expr): Int {
+  if (expr is Num) return expr.value
+  if (expr is Sum) return eval(expr.left) + eval(expr.right)
+  throw IllegalArgumentException("Unknown expression")
+}
+
+fun evalwhen(expr: Expr): Int {
+  return when(expr) {
+    is Num -> expr.value
+    is Sum -> eval(expr.left) + eval(expr.right)
+    else -> throw IllegalArgumentException("Unknown expression")
+  }
+}
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+fun TODO() {
+  throw NotImplementedError()
+}
+
+fun main(args: Array<String> = arrayOf()) {
+  assert(eval(Num(1)) == 1)
+  assert(eval(Sum(Num(1), Num(2))) == 3)
+  println("OK")
+}
+
