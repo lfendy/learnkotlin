85032155e88a3727c4c45e47dcd03f26b74d6176
Liauw Fendy <lfendy@some.place>
2020-01-21 15:58:46 +1100

    Convention, exercise 6 - destructuring

---
+++ koan/b06_destructuring/b06_destructuring.kt
@@ -0,0 +1,36 @@
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+
+fun isLeapDay(date: MyDate): Boolean {
+
+  // Two ways of destructuring:
+  // 1. positional destructuring
+	// val (year, month, dayOfMonth) = date
+
+  // 2. using run to destructure, 
+  //  will allow for mismatch between index position in class' constructor, because the right part of the statement actually uses variables
+  //  however, the position need to be the same between left side of statement and right side
+	// val (month, year, dayOfMonth) = date.run { arrayOf(month, year, dayOfMonth) }
+
+	// 29 February of a leap year
+	// return year % 4 == 0 && month == 2 && dayOfMonth == 29
+
+  // run     (no send, return last)
+  // return run { date.year % 4 == 0 && date.month == 2 && date.dayOfMonth == 29 }
+
+  // with    (send this, return last)
+  // return with(date) { year % 4 == 0 && month == 2 && dayOfMonth == 29 }
+
+  // T.run   (extension fun, send this, return last)
+  // return date.run { year % 4 == 0 && month == 2 && dayOfMonth == 29 }
+
+  // T.let   (extension fun, send it, return last)
+  return date.let { it.year % 4 == 0 && it.month == 2 && it.dayOfMonth == 29 }
+
+  // T.apply (send this, return this)
+  // T.also  (send it, return this)
+}
+
+fun main(args: Array<String> = arrayOf()) {
+	println(isLeapDay(MyDate(2020, 2, 29)))
+}
+
